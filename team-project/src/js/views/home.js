39743d74a5054b8f0f488adacbf7285ac5c9ed11
Bilanuk <bilanuk.bill@gmail.com>
2023-05-24 13:29:05 +0300

    add home content

--- team-project/src/js/views/home.js
+++ team-project/src/js/views/home.js
@@ -8,7 +8,11 @@ export default () => /*html*/`
         </div>
         <div class="col-5 col-m-7 col-s-12 page-block main-content">
             <h1>Home</h1>
-            <!-- <users-list></users-list> -->
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos perferendis commodi, odit minus atque recusandae optio fugiat eaque reiciendis id saepe quisquam temporibus, quam necessitatibus neque, explicabo accusantium illum aliquid.
+            Alias minus laudantium explicabo, vitae ea, eius, minima quae ipsum perferendis deleniti aspernatur tempore id fuga dolorem ad dolorum vero deserunt earum voluptate. Obcaecati voluptatem aperiam sed atque explicabo vel.
+            Ratione quam fugiat repudiandae! Neque ratione pariatur dicta tenetur quia ipsum alias. Ipsa ea assumenda dolores suscipit repellendus exercitationem quasi consectetur vero consequatur? Ea assumenda praesentium libero minus, non doloremque.
+            Totam voluptatum tenetur consequuntur velit blanditiis distinctio, ipsa sequi, molestiae culpa deleniti ipsam maxime aperiam atque laboriosam voluptas nihil repellat et vitae saepe illo dolorum ullam! Rem praesentium deleniti quam.
+            Laboriosam, commodi. Fugit itaque fugiat, officiis eligendi ipsam non ipsum soluta, modi rem repellendus sapiente? Culpa, nihil in, tempora iure pariatur ipsam reiciendis nemo, doloremque magnam quasi consectetur ad! Molestiae!</p>
         </div>
     </div>
    
